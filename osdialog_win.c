9d4bf9eb86055d1ff6b4a1cd203659fac24646fd
NicolasFirmo <49406269+NicolasFirmo@users.noreply.github.com>
2019-11-14 22:12:28 -0300

    snwprintf to snprintf

--- osdialog_win.c
+++ osdialog_win.c
@@ -117,7 +117,7 @@ char *osdialog_file(osdialog_file_action action, const char *path, const char *f
 		wchar_t strFile[MAX_PATH] = L"";
 		if (filename) {
 			wchar_t *filenameW = utf8_to_wchar(filename);
-			snwprintf(strFile, MAX_PATH, L"%s", filenameW);
+			snprintf(strFile, MAX_PATH, L"%s", filenameW);
 			OSDIALOG_FREE(filenameW);
 		}
 		ofn.lpstrFile = strFile;
